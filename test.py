f35708bdbfc2ef66f13d02d73282f8472ba0255e
Nathan Dataguake Basanese <v6@users.noreply.github.com>
2016-02-02 21:28:04 -0800

    // , Add comment.
      // ,  This comment links to an image about differences between Python 3 and Python 2.

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
-x=input("enter the number")
+x=input("enter the number") # This works differently in Python 3. http://www.python-course.eu/images/input_difference_python2_python3.png
 print ("hello world")
